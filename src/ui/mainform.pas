1a0a8395632399cab4c718111ab19a3e6d360d88
martin-damien <168558789+martin-damien@users.noreply.github.com>
2026-07-15 21:56:36 +0200

    POC LCL rendering

--- src/ui/mainform.pas
+++ src/ui/mainform.pas
@@ -5,7 +5,7 @@ unit mainform;
 interface
 
 uses
-    Classes, SysUtils, Forms, Controls, Graphics, Dialogs, MrtfDocument;
+    Classes, SysUtils, Forms, Controls, Graphics, Dialogs, MrtfDocument, LazUTF8;
 
 type
 
@@ -13,14 +13,20 @@ type
 
     TApplicationForm = class(TForm)
         procedure FormCreate(Sender: TObject);
-    private
+        procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+        procedure FormPaint(Sender: TObject);
 
-    public
+        private
+
+        public
 
     end;
 
 var
     ApplicationForm: TApplicationForm;
+    FText: string;
+    FCaret: integer;
+    x, y: integer;
 
 implementation
 
@@ -29,17 +35,45 @@ implementation
 { TApplicationForm }
 
 procedure TApplicationForm.FormCreate(Sender: TObject);
-var
-    doc: TDocument;
 begin
-    doc := TDocument.Create;
-    doc.AddParagraph;
-    doc.AddParagraph;
-    try
-        ShowMessage('Document created with ' + IntToStr(doc.ParagraphsCount) + ' paragraphs');
-    finally
-        doc.Free;
-    end;
+    {
+        We will use UTF-8 and count in "unicode code points",
+        "grapheme" will not be handled (look é  it count as two code points)
+    }
+    FText := 'Hello World é 🙂 AVAVAVAV ToToToTo';
+    FCaret := 0;
+    y := 10;
+end;
+
+procedure TApplicationForm.FormKeyDown(Sender: TObject; var Key: Word;
+    Shift: TShiftState);
+begin
+    inc(FCaret);
+    Invalidate;
+end;
+
+procedure TApplicationForm.FormPaint(Sender: TObject);
+var
+    textToCaret: string;
+    caretX, caretY: integer;
+    textHeight: integer;
+begin
+    Canvas.TextOut(10, 10, FText);
+
+    { Cursor }
+
+    textToCaret := UTF8Copy(FText, 1, FCaret);
+    textHeight := Canvas.TextHeight(textToCaret);
+
+    x := 10 + Canvas.TextWidth(textToCaret);
+
+    caretX := 10 + Canvas.TextWidth(textToCaret);
+    caretY := y;
+
+    Canvas.MoveTo(caretX, caretY);
+    Canvas.LineTo(caretX, caretY + 20);
+
+    Caption := Format('%d %d %d', [FCaret, x, y]);
 end;
 
 end.
